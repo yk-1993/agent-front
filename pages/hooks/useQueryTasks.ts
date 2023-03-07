1684b9aee4e59a348eaf1c8f63b42bfb9ae6e7ed
YusukeKimura <litbus19930529@yahoo.co.jp>
2023-03-08 02:22:17 +0900

    react-queryでタスクのCRUD追加

--- pages/hooks/useQueryTasks.ts
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router'
-import axios from 'axios'
-import { useQuery } from '@tanstack/react-query'
-import { Task } from '@prisma/client'
-
-export const useQueryTasks = () => {
-    return <div>useQueryTasks</div>
-}
